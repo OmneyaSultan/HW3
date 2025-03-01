469326b8c99cef1b4c3a038e01d8474393eb27df
Omneya Sultan <113146260+OmneyaSultan@users.noreply.github.com>
2025-03-01 15:41:40 -0800

    Fix evaluator

--- evaluator.py
+++ evaluator.py
@@ -15,12 +15,12 @@ def compare(to_score, truth):
     reference_possibilities = reference.split('\t')
     if any(simplify_text(word.lower().strip()) in simplify_text(answer.lower().strip()) for word in reference_possibilities):
       correct += 1
-    return correct / len(truth)
+  return correct / len(truth)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", type=str, required=True, default = "datasets/results_old/dev_gemma2_7b_it_faiss_rr.answers.txt")
-    parser.add_argument("-t", type=str, required=True, default = "datasets/answers.dev.txt")
+    parser.add_argument("-p", type=str, default = "datasets/experiments/swap_retriever_same_generator/dev_gemma2_7b_it_faiss_rr.answers.txt")
+    parser.add_argument("-t", type=str, default = "datasets/answers.dev.txt")
     args = parser.parse_args()
 
     with open(args.p, "r") as f:
